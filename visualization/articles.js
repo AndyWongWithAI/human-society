1d789dbbc6013ca9c2dc843421a0c917b584b28f
hq <AndyWongWithAI@users.noreply.github.com>
2026-07-16 19:48:45 +0800

    feat: 添加 DED-042 公众号文章《算法透明才是数字时代最坚硬的铠甲》
    via [HAPI](https://hapi.run)
    
    Co-Authored-By: HAPI <noreply@hapi.run>

--- visualization/articles.js
+++ visualization/articles.js
@@ -1,4 +1,24 @@
 window.ARTICLE_DATA = {
-  "articles": [],
-  "entity_articles": {}
+  "articles": [
+    {
+      "url": "https://mp.weixin.qq.com/s/bgKQiO8qvrwuBs_8_oFxqQ",
+      "title": "算法透明才是数字时代最坚硬的铠甲",
+      "date": "2026-07-16",
+      "entity_ids": [
+        "DED-042"
+      ]
+    }
+  ],
+  "entity_articles": {
+    "DED-042": [
+      {
+        "url": "https://mp.weixin.qq.com/s/bgKQiO8qvrwuBs_8_oFxqQ",
+        "title": "算法透明才是数字时代最坚硬的铠甲",
+        "date": "2026-07-16",
+        "entity_ids": [
+          "DED-042"
+        ]
+      }
+    ]
+  }
 };
